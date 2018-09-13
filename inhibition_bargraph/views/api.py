375f5948ceb6b0505c1e086dbc6434a17808416c
Radu Suciu <radusuciu@gmail.com>
2018-09-13 16:38:23 -0700

    Serve basic plot

--- inhibition_bargraph/views/api.py
+++ inhibition_bargraph/views/api.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, send_file
+from inhibition_bargraph.core.plot import plot_horizontal
+import inhibition_bargraph.core.whitelist as whitelist
+import inhibition_bargraph.core.blacklist as blacklist
 import inhibition_bargraph.api as api
 
 
@@ -9,3 +12,17 @@ api_blueprint = Blueprint('api_blueprint', __name__,
 @api_blueprint.route('/')
 def index():
     return jsonify('hello')
+
+@api_blueprint.route('/plot/<path:url>')
+def generate_plot(url):
+    memory_file = plot_horizontal(
+        source_url=url,
+        name='wat',
+        whitelist=whitelist.human_serine_hydrolases,
+        blacklist=blacklist.human_serine_hydrolases
+    )
+
+    return send_file(
+        memory_file,
+        attachment_filename='wat.svg'
+    )
