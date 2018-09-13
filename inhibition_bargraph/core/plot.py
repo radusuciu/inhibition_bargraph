615f937aa6c9f40d659cd0b9e15a48d4ece4483d
Radu Suciu <radusuciu@gmail.com>
2018-09-13 15:52:30 -0700

    Pare down plot for single dataset

--- inhibition_bargraph/core/plot.py
+++ inhibition_bargraph/core/plot.py
@@ -1,45 +1,57 @@
-from collections import defaultdict, namedtuple
-from copy import deepcopy
-import operator
+import inhibition_bargraph.api as api
 import numpy as np
 import matplotlib
-
 matplotlib.use('Agg')
-
 import matplotlib.pyplot as plt
-import itertools
-import numbers
-import datetime
-import requests
-import pathlib
-import numbers
-import csv
-import sys
-import json
 
 matplotlib.rcParams['font.sans-serif'] = "Arial"
 matplotlib.rcParams['font.family'] = "sans-serif"
 
 
-blacklist = [
-    'P35030', 'P07477', 'P07478', # trypsins 
-    'O15427', # SLC16A3
-    'P00734', # F2
-    'Q14703', # MBTSP1 (serine protease)
-    'Q8NBP7', # PCSK9
-]
+class PlotData:
+    def __init__(self, source_url, name=None):
+        self.source_url = source_url
+        self.name = name
+        self._data = self.init_data()
+
+    @property
+    def data(self):
+        # slicing from one index because the first index is uniprot
+        # zipping into three tuples containing list of symbols, ratios, stdevs
+        return tuple(zip(*(r[1:] for r in self._data)))
+
+    def init_data(self):
+        """Helper initialization method which creates experiment if needed."""
+        experiment = api.get_experiment(self.source_url) or api.new_experiment(self.source_url, self.name)
+        if not self.name:
+            self.name = experiment.name
+        return api.get_dataset(experiment.experiment_id)
+
+    def apply_whitelist(self, whitelist=None):
+        if whitelist:
+            self._data = (r for r in self._data if r[0] in whitelist)
+
+    def apply_blacklist(self, blacklist=None):
+        if blacklist:
+            self._data = (r for r in self._data if r[0] not in blacklist)
 
 
-def plot_horizontal(plot_name, datasets):
-    data = format_data_for_plots(datasets)
-    symbols = data.pop(0)
+def plot_horizontal(source_url, name=None, whitelist=None, blacklist=None):
+    all_data = PlotData(source_url, name)
+    all_data.apply_whitelist(whitelist)
+    all_data.apply_blacklist(blacklist)
+    symbols, ratios, stdevs = all_data.data
+
+    if not name:
+        name = all_data.name
 
     N = len(symbols)
 
     ind = np.arange(N)
-    height = 0.5 - (0.1 * len(datasets))
+    height = 0.4
 
     fig, ax = plt.subplots()
+    fig.set_size_inches(4, 8, forward=True)
 
     error_kw = {
         'elinewidth': 1,
@@ -47,47 +59,22 @@ def plot_horizontal(plot_name, datasets):
         'capthick': 0.5
     }
 
-    fig.set_size_inches(4, 8, forward=True)
-
-    colors = ['k', 'r', 'c']
-
-    def grouper(iterable, n, fillvalue=None):
-        "Collect data into fixed-length chunks or blocks"
-        # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
-        args = [iter(iterable)] * n
-        return itertools.zip_longest(*args, fillvalue=fillvalue)
-
-    for i, (ratios, stderr) in enumerate(grouper(data, 2)):
-        pos = ind + (height + 0.1) * i
-        label = '{} - {}'.format(datasets[i].inhibitor, datasets[i].concentration)
-        ax.barh(pos, ratios, height, xerr=stderr, error_kw=error_kw, color=colors.pop(0), label=label)
+    pos = ind + (height + 0.1)
+    ax.barh(pos, ratios, height, xerr=stdevs, error_kw=error_kw, color='k', label=name)
 
     # add some text for labels, title and axes ticks
     ax.set_xlabel('Ratio')
 
-    yticks = ind
-
-    if len(datasets) == 1:
-        yticks = yticks
-    elif len(datasets) == 2:
-        yticks = yticks + height/2 + 0.05
-    elif len(datasets) == 3:
-        yticks = yticks + height + 0.1
-
-    ax.set_yticks(yticks)
-
+    ax.set_yticks(ind)
     ax.tick_params(direction='out')
     ax.get_xaxis().tick_bottom()
     ax.get_yaxis().tick_left()
-
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.set_yticklabels(symbols, size=8)
-
     ax.legend(frameon=False, prop={'size': 11}, loc='lower right')
-
     plt.xlim(0, 2)
     plt.ylim(-1)
     plt.tight_layout()
 
-    plt.savefig(str(DATA_OUTPUT_PATH.joinpath(plot_name)), format='svg')
+    plt.savefig('test.svg', format='svg')
