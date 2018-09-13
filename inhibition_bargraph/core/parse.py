2f0e279878cddf3c519078e778ba418b8c333857
Radu Suciu <radusuciu@gmail.com>
2018-09-12 19:33:19 -0700

    Simplify parsing function

--- inhibition_bargraph/core/parse.py
+++ inhibition_bargraph/core/parse.py
@@ -12,13 +12,9 @@ headers: Headers = [
     'stdev'
 ]
 
-def flatten_dataset(url: str) -> Tuple[Headers, Dataset]:
-    """Flattens a dataset by removing entries for individual peptides."""
-    raw_dataset: str = get_dataset_from_url(url)
+def get_dataset_from_url(url: str) -> Tuple[Headers, Dataset]:
+    """Get raw dataset from a given url."""
+    raw_dataset: str = requests.get(url).text
     trimmed = (x.split('\t') for x in raw_dataset.splitlines() if x[0].isdigit())
     mapped = ([y for y in x if y != ' '][1: len(headers) + 1] for x in trimmed)
     return (headers, mapped)
-
-def get_dataset_from_url(url: str) -> str:
-    """Get raw dataset from a given url."""
-    return requests.get(url).text
