adbcd887814a8bd08927d062c47ac93e81641640
Radu Suciu <radusuciu@gmail.com>
2018-09-13 15:51:50 -0700

    Add predefined whitlists and blacklists

---
+++ inhibition_bargraph/core/blacklist.py
@@ -0,0 +1,9 @@
+"""Contains lists of undesirable uniprots for common combinations of species and enzyme classes."""
+
+human_serine_hydrolases = [
+    'P35030', 'P07477', 'P07478', # trypsins 
+    'O15427', # SLC16A3
+    'P00734', # F2
+    'Q14703', # MBTSP1 (serine protease)
+    'Q8NBP7', # PCSK9
+]
