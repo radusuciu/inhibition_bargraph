3310acf8a4f58495619d5c589b4ebb2622a461e3
Radu Suciu <radusuciu@gmail.com>
2018-09-13 22:58:39 -0700

    Pass experiment name through secure_filename  + minor changes

--- inhibition_bargraph/views/api.py
+++ inhibition_bargraph/views/api.py
@@ -1,17 +1,12 @@
 from flask import Blueprint, jsonify, send_file, request
+from werkzeug import secure_filename
 from inhibition_bargraph.core.plot import plot_horizontal
 import inhibition_bargraph.core.whitelist as whitelist
 import inhibition_bargraph.core.blacklist as blacklist
 import inhibition_bargraph.api as api
 
 
-api_blueprint = Blueprint('api_blueprint', __name__,
-                  template_folder='templates',
-                  static_folder='static')
-
-@api_blueprint.route('/')
-def index():
-    return jsonify('hello')
+api_blueprint = Blueprint('api_blueprint', __name__)
 
 @api_blueprint.route('/plot/<path:url>')
 def generate_plot(url):
@@ -19,7 +14,7 @@ def generate_plot(url):
 
     memory_file = plot_horizontal(
         source_url=url,
-        name=name,
+        name=secure_filename(name),
         whitelist=whitelist.human_serine_hydrolases,
         blacklist=blacklist.human_serine_hydrolases
     )
