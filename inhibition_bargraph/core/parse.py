9b33d6bf60363bb4cbc6e33254de0df7acd21692
Radu Suciu <radusuciu@gmail.com>
2018-09-13 22:06:21 -0700

    Parse out individual peptide entries

--- inhibition_bargraph/core/parse.py
+++ inhibition_bargraph/core/parse.py
@@ -1,20 +1,46 @@
+from typing import Union, List, Iterable, Tuple, Dict
+import itertools
 import requests
-from typing import Union, List, Iterable, Tuple
 
-Dataset = Iterable[Union[str, int]]
+DataItem = Union[str, int, float]
 Headers = List[str]
 
-headers: Headers = [
-    'uniprot',
-    'description',
-    'symbol',
-    'median',
-    'stdev'
-]
 
-def get_dataset_from_url(url: str) -> Tuple[Headers, Dataset]:
-    """Get raw dataset from a given url."""
+entry_header_index_map: Dict[str, int] = {
+    'uniprot': 1,
+    'description': 2,
+    'symbol': 3,
+}
+
+entry_index_map: Dict[str, int] = {
+    'sequence': 0,
+    'mass': 1,
+    'ratio': 2,
+    'stats': 3,
+    'run': 6,
+    'charge': 7,
+    'segment': 8,
+    'link': 9
+}
+
+headers: Headers = list(entry_header_index_map.keys()) + list(entry_index_map.keys())
+entry_header_indices: Tuple[int] = tuple(entry_header_index_map.values())
+entry_indices: Tuple[int] = tuple(entry_index_map.values())
+
+def get_dataset_from_url(url: str) -> Tuple[Headers, List[DataItem]]:
+    """Get raw dataset from  given url."""
     raw_dataset: str = requests.get(url).text
-    trimmed = (x.split('\t') for x in raw_dataset.splitlines() if x[0].isdigit())
-    mapped = ([y for y in x if y != ' '][1: len(headers) + 1] for x in trimmed)
-    return (headers, mapped)
+
+    flattened_dataset: List[DataItem] = []
+    entry_header_data: List[str] = []
+
+    for line in raw_dataset.splitlines()[1:]:
+        split_line: List[DataItem] = [i for i in line.split('\t') if i.strip()]
+        if split_line[0].isdigit():
+            entry_header_data = [split_line[i] for i in entry_header_indices]
+        else:
+            flattened_dataset.append(
+                entry_header_data + [split_line[i] for i in entry_indices]
+            )
+
+    return (headers, flattened_dataset)
