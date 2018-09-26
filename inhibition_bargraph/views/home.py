e023e3f0336d9fefd8344e3d2f258fbcf979512d
Radu Suciu <radusuciu@gmail.com>
2018-09-26 15:11:15 -0700

    Insert plot herf into template as needed

--- inhibition_bargraph/views/home.py
+++ inhibition_bargraph/views/home.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask import request
 from inhibition_bargraph import db
 
 home = Blueprint('home', __name__,
@@ -10,7 +11,15 @@ home = Blueprint('home', __name__,
 @home.route('/')
 def index():
     bootstrap = None
-    return render_template('index.html', bootstrap=bootstrap)
+    plot_href = None
+
+    if request.args.get('source_url'):
+        keys_to_pass = ('type', 'name', 'inverse', 'orientation')
+        args_to_pass = '&'.join([ '{}={}'.format(a, request.args.get(a)) for a in keys_to_pass if request.args.get(a) ])
+        plot_href = '/api/plot/{}?{}'.format(request.args.get('source_url'), args_to_pass)
+        print(plot_href)
+
+    return render_template('index.html', bootstrap=bootstrap, plot_href=plot_href)
 
 @home.before_app_first_request
 def create_db():
